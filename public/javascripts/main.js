53a1e440ca2434cd468074463fd1a0a622d5706a
Jan Fiedler <janfiedler@gmail.com>
2017-10-17 18:22:00 +0200

    0.0.6 Release

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -149,9 +149,9 @@ $( document ).ready(function() {
     socket.on('queuePosition', function (data) {
         //console.log(data);
         if(data.position>1){
-            $('#mineLog').prepend('<div><small>'+new Date().toISOString()+':</small> &nbsp;&nbsp;Your withdrawal request is '+data.position+'th in a row. One request takes approx. 2 minutes. Please wait.</div>');
+            $('#mineLog').prepend('<div><small>'+new Date().toISOString()+':</small> &nbsp;&nbsp;Your withdrawal request is '+data.position+'th in a row. One request takes approx. between  10 - 20 seconds. Please wait.</div>');
         } else {
-            $('#mineLog').prepend('<div><small>'+new Date().toISOString()+':</small> &nbsp;&nbsp;Your request is now in progress. Wait on transaction data (approx. 2 minutes).</div>');
+            $('#mineLog').prepend('<div><small>'+new Date().toISOString()+':</small> &nbsp;&nbsp;Your request is now in progress. Wait on transaction data.</div>');
         }
     });
     socket.on('minersOnline', function (data) {
