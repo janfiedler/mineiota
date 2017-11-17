2ee0d93b860fd339501cf62e1b97b26b0c837585
Jan Fiedler <janfiedler@gmail.com>
2017-11-17 16:50:36 +0100

    Do not need default filedb table.json anymore, now if is missing is created with default data.

--- filedb/app.js
+++ filedb/app.js
@@ -4,7 +4,31 @@ const folder = "filedb/";
 // ========
 module.exports = {
     select: function (table) {
-        return JSON.parse(fs.readFileSync(folder+table+'.json', 'utf8'));
+        if (!fs.existsSync(folder+table+'.json')) {
+            var tableDefault = "";
+             switch(String(table)) {
+                case "keyIndex":
+                    tableDefault = '{"data":0}';
+                    this.update(table, JSON.parse(tableDefault));
+                    return JSON.parse(tableDefault);
+                    break;
+                case "cache":
+                    tableDefault = '{"withdrawalInProgress":false,"isReattachable":null,"resetUserBalanceList":[],"trytes":[],"bundleHash":null}';
+                    this.update(table, JSON.parse(tableDefault));
+                    return JSON.parse(tableDefault);
+                    break;
+                case "queue":
+                    tableDefault = '{"ids":[],"addresses":[]}';
+                    this.update(table, JSON.parse(tableDefault));
+                    return JSON.parse(tableDefault);
+                    break;
+                default:
+                    tableDefault = '{"default":"Unknown table"}';
+                    return JSON.parse(tableDefault);
+            }
+        } else {
+            return JSON.parse(fs.readFileSync(folder+table+'.json', 'utf8'));
+        }
     },
     update: function (table, json) {
         fs.writeFileSync(folder+table+'.json', JSON.stringify(json), 'utf8');
